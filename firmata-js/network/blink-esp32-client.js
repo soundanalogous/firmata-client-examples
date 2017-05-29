8a943c189957b16cc3ac1f789fdd91ca367c98cd
Jeff Hoefs <jeff.hoefs@gmail.com>
2017-05-28 23:32:34 -0700

    add esp32 WiFi examples

---
+++ firmata-js/network/blink-esp32-client.js
@@ -0,0 +1,48 @@
+/*
+ * To run in Debug mode: DEBUG=etherport-client node wifi-test
+ */
+var Firmata = require("firmata").Board;
+var EtherPortClient = require("etherport-client").EtherPortClient;
+var board = new Firmata(new EtherPortClient({
+  host: "192.168.86.42",
+  port: 3030
+}), {
+  samplingInterval: 100 // decrease analog read frequency
+});
+
+// may take several seconds to establish a connection
+process.stdout.write("connecting");
+var connectionProgress = setInterval(function() {
+  process.stdout.write(".");
+}, 500);
+
+board.on("ready", function() {
+  clearInterval(connectionProgress);
+  console.log("\nREADY!");
+  console.log(
+    board.firmware.name + "-" +
+    board.firmware.version.major + "." +
+    board.firmware.version.minor
+  );
+
+  // onboard LED and button
+  var ledPin = 5;
+  var buttonPin = 0;
+  var buttonState = 1;
+
+  this.pinMode(ledPin, this.MODES.OUTPUT);
+  this.pinMode(buttonPin, this.MODES.INPUT);
+
+  setInterval(function() {
+    this.digitalWrite(ledPin, (buttonState ^= 1));
+  }.bind(this), 500);
+
+  board.digitalRead(buttonPin, function (val) {
+    console.log("pin " + buttonPin + " value = " + val);
+  });
+
+  this.analogRead(0, function(val) {
+    console.log(val);
+  });
+
+});
